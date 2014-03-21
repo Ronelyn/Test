4f9360ab17354f773e267bd917bc60946d23b91f
Fran Stewart <christinest@vmc.com>
2014-03-21 14:38:47 -0700

    Finished version of front-page shopping cart.

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -46,7 +46,7 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
+        format.html { redirect_to @line_item }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -60,7 +60,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to @line_item.cart, notice: 'Product removed.' }
+      format.html { redirect_to line_items_url }
       format.json { head :no_content }
     end
   end
@@ -75,4 +75,5 @@ class LineItemsController < ApplicationController
     def line_item_params
       params.require(:line_item).permit(:product_id)
     end
+  #...
 end
--- depot/app/helpers/application_helper.rb
+++ depot/app/helpers/application_helper.rb
@@ -4,5 +4,12 @@ module ApplicationHelper
     session[:counter, 1] if session[:counter].nil?
     session[:counter]
   end
+  
+  def generate_hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
 
 end
